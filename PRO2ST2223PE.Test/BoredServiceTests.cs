da8e91023377e469d9b0ea4db197f8e3378fcde2
YJouhar47 <youssef.jouhar1998@hotmail.com>
2022-11-04 15:42:46 +0100

    Afgemaakte versie

--- PRO2ST2223PE.Test/BoredServiceTests.cs
+++ PRO2ST2223PE.Test/BoredServiceTests.cs
@@ -6,17 +6,40 @@ namespace PRO2ST2223PE.Test
     public class BoredServiceTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void BoredVullen_Of_Er_10_Variabalen_Toegevoegd_Zijn()
         {
-            //Arrange
             var boredServiceApi = new Mock<IBoredApiService>();
-            boredServiceApi.Setup(x => x.GetBoredResponse().Activity).Returns("Go swimming with a friend");
-
+            boredServiceApi.Setup(x => x.GetBoredResponse()).Returns(new BoredResponse() { });
             var service = new BoredService(boredServiceApi.Object);
-            var result = service.BoredRandom();
+            service.BoredVullen();
+            var expected = new List<BoredResponse>();
+            for (int i = 0; i < 10; i++)
+            {
+                expected.Add(new BoredResponse() { });
+            }
+            Assert.AreEqual(expected.Count, BoredInMemoryDb.boredResponses.Count);
+        }
 
-            Assert.AreEqual("Go swimming with a friend", result);
-            
+        [TestMethod]
+        public void BoredRandom_Kleiner_Dan_Twee()
+        {
+            var boredServiceApi = new Mock<IBoredApiService>();
+            boredServiceApi.Setup(x => x.GetBoredResponse()).Returns(new BoredResponse() { Activity = "Organize your music collection", Participants = 1 });
+            var service = new BoredService(boredServiceApi.Object);
+            service.BoredVullen();
+            var expected = "I don't have enough friends to => Organize your music collection";
+            Assert.AreEqual(expected, service.BoredRandom());
+        }
+
+        [TestMethod]
+        public void BoredRandom_Groter_Dan_Twee()
+        {
+            var boredServiceApi = new Mock<IBoredApiService>();
+            boredServiceApi.Setup(x => x.GetBoredResponse()).Returns(new BoredResponse() { Activity = "Go see a Broadway production", Participants = 4 });
+            var service = new BoredService(boredServiceApi.Object);
+            service.BoredVullen();
+            var expected = "Let's do some => Go see a Broadway production";
+            Assert.AreEqual(expected, service.BoredRandom());
         }
     }
 }
--- PRO2ST2223PE/BoredService.cs
+++ PRO2ST2223PE/BoredService.cs
@@ -2,12 +2,9 @@
 
 namespace PRO2ST2223PE;
 
-public class BoredService
+public class BoredService : IBoredService
 {
-    public BoredService() 
-    {
-        
-    }
+    public BoredService(){}
     private readonly IBoredApiService _boredServiceApi;
     public BoredService(IBoredApiService boredServiceApi)
     {
@@ -15,19 +12,18 @@ public class BoredService
     }
     public void BoredVullen()
     {
-        BoredApiService BoredApiService = new BoredApiService();
 
         BoredInMemoryDb.boredResponses.Clear();
         for (int i = 0; i < 10; i++)
         {
-            BoredInMemoryDb.boredResponses.Add(BoredApiService.GetBoredResponse());
+            BoredInMemoryDb.boredResponses.Add(_boredServiceApi.GetBoredResponse());
         }
     }
     public string BoredRandom()
     {
         Random random = new Random();
         BoredResponse boredResponse = BoredInMemoryDb.boredResponses[random.Next(10)];
-        if (boredResponse.Participants > 2)
+        if (boredResponse.Participants < 2)
         {
             return $"I don't have enough friends to => {boredResponse.Activity}";
         }
